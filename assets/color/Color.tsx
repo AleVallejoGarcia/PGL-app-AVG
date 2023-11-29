50dccd9d9f19d6609b449d56855e5308b682b678
AleVallejoGarcia <alevalga@gmail.com>
2023-11-29 09:35:37 +0000

    feat: creacion de welcome

---
+++ assets/color/Color.tsx
@@ -0,0 +1,7 @@
+export const colors = {
+  Black: "#000000",
+  Cordovan: "#883444",
+  DavysGray: "#5D5966",
+  FrenchGray: "#C1C0CC",
+  Dun: "#E8D4BC",
+};
---
+++ components/Welcome.tsx
@@ -0,0 +1,60 @@
+import { ImageBackground, StyleSheet, Text, View, Pressable } from 'react-native'
+import { colors } from '../assets/color/Color';
+import React from 'react'
+
+
+export default function Welcome() {
+    return (
+    
+    <View style={styles.container}>
+      <ImageBackground source={require('../assets/images/fondoApp.jpg')} resizeMode="cover" style={styles.image}>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>Welcome Back User</Text>
+          <Pressable style={styles.login}>
+            <Text style={styles.textLogin}>Login</Text>
+          </Pressable>
+        </View>
+      </ImageBackground>
+    </View>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 20,
+  },
+  image: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  textContainer: {
+    marginTop: '30%',
+    width: '70%',
+    height: 220,
+    borderRadius: 20,
+    alignItems: 'center',
+    backgroundColor: colors.Cordovan,
+    justifyContent: 'space-around'
+  },
+  text: {
+    color: colors.Dun,
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center', 
+  },
+  login: {
+    verticalAlign: 'bottom',
+    padding: 10,
+    paddingHorizontal: 45,
+    borderRadius: 30,
+    backgroundColor: colors.Dun, 
+  },
+  textLogin: {
+    color: colors.Black,
+    fontWeight: 'bold',
+    fontSize: 18
+  }
+});
